33cd3506a306536dc374460398f42a8190d1d406
Shreekar Hegde <shreekarhegde9@gmail.com>
2018-12-21 16:40:14 +0530

    updating

--- models/url.js
+++ models/url.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const sh = require("shorthash");
 
-
 const Schema = mongoose.Schema;
 
 const urlSchema = new Schema({
@@ -60,18 +59,6 @@ urlSchema.pre('save', function (next) {
 
 const Url = mongoose.model('Url', urlSchema);
 
-
 module.exports = {
     Url
 };
-
-//create new url
-// let url = new Url({
-//     title: "How to be become a full stack developer", originalURL: "https://dctacademy.com/2018/05/become-full-stack-javascript-developer/", tags: ["javascript", "mern", "full stack developer"]
-// });
-
-// url.save().then((url) => {
-//     console.log(url);
-// }).catch((err) => {
-//     console.log(err);
-// });
